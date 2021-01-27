150b25fd0ca690eb911c0253b6ab66e3e72b7f7e
Vinicius Monteiro <vinidsmonteiro@gmail.com>
2021-01-27 16:48:57 +0000

    no need to declare controller from the wrapper. Autoconfiguration is not enabled

--- src/main/java/com/vinimo/product/service/ProductServiceApplication.java
+++ src/main/java/com/vinimo/product/service/ProductServiceApplication.java
@@ -3,14 +3,12 @@ package com.vinimo.product.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
